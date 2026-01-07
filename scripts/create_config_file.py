87ad2f2d5303410b57e7f1c901560f71ce0014fe
Ysmael Clemente <skategrama@gmail.com>
2026-01-07 15:39:59 -0500

    env variable added

--- scripts/create_config_file.py
+++ scripts/create_config_file.py
@@ -11,6 +11,7 @@ class ConfigFile:
             "db_port": os.getenv("DB_PORT", 5432),
             "db_user": os.getenv("DB_USER", "postgres"),
             "db_password": os.getenv("DB_PASSWORD", "postgres"),
+            "db_name": os.getenv("DB_NAME", None),
             "dbfilter": os.getenv("DB_FILTER", "odoo_*"),
             "addons_path": "/usr/lib/python3/dist-packages/odoo/addons,/mnt/extra-addons,/mnt/enterprise-addons",
             "list_db": os.getenv("LIST_DB", True),
